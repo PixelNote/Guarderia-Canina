b1177554432738a8935b692ddd1fb8fa58d7c7a6
Andrés Jiménez <andresito_s2@live.com>
2023-05-21 19:00:43 -0500

    Restore jacksonConverter

--- src/main/java/com/example/guarderia/service/rabbitmq/NotificationDTO.java
+++ src/main/java/com/example/guarderia/service/rabbitmq/NotificationDTO.java
@@ -1,5 +1,8 @@
 package com.example.guarderia.service.rabbitmq;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +18,9 @@ public class NotificationDTO {
     String client;
     String email;
     String pet;
+    @JsonSerialize(using = LocalDateSerializer.class)
     LocalDate date;
+    @JsonSerialize(using = LocalTimeSerializer.class)
     LocalTime time;
 
 
